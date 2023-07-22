01f9398650c6fd998b710d303a7728db1e4be1b0
alton peel <altonpeel123@gmail.com>
2023-07-22 15:08:09 -0400

    finishing up moms shopping list

--- level1/moms-shopping-list/index.js
+++ level1/moms-shopping-list/index.js
@@ -1,18 +1,54 @@
 const form = document["shoppingList"]
 var food = document.getElementById("food")
+var newText = document.getElementById("newText")
 
-form.addEventListener("submit", function (e){
+
+
+form.addEventListener("submit", (e) => {
     e.preventDefault()
     const cart = form.cart.value
     
-    var newItem = document.createElement("li")
-    newItem.innerHTML = "<input type='checkbox'  value='cart[i]'/>"+ " " + cart + " " + "<button> x </button>" 
-    form.append(newItem)
+    const newItem = document.createElement("li")
+    newItem.textContent = cart
+    newItem.setAttribute("id" , "listItem")
+    food.append(newItem)
     newItem.style.textAlign = "center"
 
     shoppingList.cart.value = ""
     console.log(cart)
-})
+    
+    const delBtn = document.createElement("button")
+    delBtn.innerText = "delete"
+    delBtn.setAttribute("id", "delBtn")
+    newItem.append(delBtn)
+    
+    delBtn.addEventListener("click", (e) => {  
+        document.getElementById("listItem").remove(newItem)
+    })    
+    
+    
+    const editBtn = document.createElement("button")
+    editBtn.innerText = "edit"
+    editBtn.setAttribute("id", "editBtn")
+    newItem.append(editBtn)
+    
+    
+    editBtn.addEventListener("click", (e) => {
+        document.getElementById("listItem").textContent = newText
+    })
+    
+    const edit = document.createElement("input")
+    edit.setAttribute("id", "newText")
+    edit.setAttribute("type", "text")
+    edit.textContent = newText
+    document.getElementById("listItem").append("inupt")
+    
+})    
 
-console.log()
+//const endBtn = document.createElement("button")
+//endBtn.innerText = "change"
+//endBtn.setAttribute("id", "endBtn")
+//newItem.append(endBtn)
+
+
 
