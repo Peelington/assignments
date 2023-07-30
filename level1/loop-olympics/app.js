cb7cf7fa73eff4cfa592e0cc227bdeb3606989ad
alton peel <altonpeel123@gmail.com>
2023-07-30 18:11:58 -0400

    completed loop Olymipics

---
+++ level1/loop-olympics/app.js
@@ -0,0 +1,62 @@
+// Preliminaries
+
+//1. Write a for loop that prints to the console the numbers 0 through 9.
+//var number = " ";
+//
+//for (var i = 0; i < 100; i++){
+//    number = i + " ";
+//    console.log(number)
+//}
+    
+    
+//2. Write a for loop that prints to the console 9 through 0.
+//  for (var i = 9; i >= 0; i--){
+//      number = i - " ";
+//          console.log(number)        
+//  }
+
+//3. Write a for loop that prints these fruits to the console.`const fruit = ["banana", "orange", "apple", "kiwi"]`
+//  const fruit = ["banana", "orange", "apple", "kiwi"]
+//    for (var i = 0; i < fruit.length; i++){
+//        console.log(fruit[i])
+//    }
+
+//# **Bronze Medal**
+
+
+//1. Write a for loop that will push the numbers 0 through 9 to an array.
+//var number = [];
+//
+//for (let i = 0; i <= 9; i++) {
+//	number.push(i);}
+//	console.log(number);
+    
+    
+    
+//2. Write a for loop that prints to the console only even numbers 0 through 100.
+//    var number = [];
+//    for (let i = 0; i <= 100; i++) {
+//        number.push(i)        
+//        if (number[i] % 2 === 0 ){
+//            console.log(number[i])
+//        }
+//    }    
+        
+              
+
+//3. Write a for loop that will push every other fruit to an array.
+//const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+//
+//function otherFruit(fruits){
+//    var evenFruit = [];
+//    for ( var i = 0; i < fruits.length; i++) {
+//        if(i % 2 === 1){
+//            evenFruit.push(fruits[i]);
+//          }
+//        }
+//    return evenFruit;
+//};
+//
+//console.log(otherFruit(["banana", "orange", "apple", "kiwi", "pear", "peach"]));
+
+
