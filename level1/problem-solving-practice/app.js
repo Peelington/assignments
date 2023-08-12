9200330255a92fd253845c5a109e40b30ed387d1
alton peel <altonpeel123@gmail.com>
2023-08-12 15:36:45 -0400

    partial problem solving practice complete

---
+++ level1/problem-solving-practice/app.js
@@ -0,0 +1,83 @@
+//Write a function that takes an array of numbers and returns the largest (without using `Math.max()`)
+
+// test data
+// function largest (arr) {
+// let largest = []
+// newArr = []
+//     for ( i = 0; i < arr.length; i++){
+//     let newArry = arr.toSorted((a, b) => a - b);
+//     newArray.pop().push().largest
+//     }
+//     }
+//         console.log(largest([6, 13, 250, 3]))
+    
+
+// function largest() { 
+// let highest = []
+//     for ( i = 0; i < highest.length; i++){
+//         highest.sort(function(a, b){return a-b});
+//         let largest = highest[highest.length-1];
+//         return largest;
+//     }
+// }   
+// console.log(largest([6, 13, 250, 3])) 
+// console.log(largest([3, 5, 2, 8, 1])) 
+// console.log(largest([-13, 40, 3, 0, 19, 22])) 
+
+
+
+
+
+
+//Write a function that takes an array of words and a character and returns each word that has that character present.
+// test data
+function lettersWithStrings(arr, str) {
+      let key = []
+      for (i = 0; i < arr.length; i++);
+      arr[i].value.match(str).push(key)
+        } 
+      console.log(key)
+
+        // if( arr.value.includes(str)){
+        //     key.push(arr)
+        // }
+    //}
+    // number.push(i);
+    // console.log(number)
+    // if(array[i] == string){
+    //     array[i].pop().push().key                    
+    // }
+
+console.log(lettersWithStrings(["$hello!", "%%^%%", "test!"], "!"))
+// => ["$hello!", "test!"]
+// console.log(lettersWithStrings(["#3", "$$$", "C%4!", "Hey!"], "!")) 
+//  => ["C%4!", "Hey!"]
+// console.log(lettersWithStrings(["yellow", "green", "^up^", "down", "dog"], "h")) 
+//  => []
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2.
+
+// function isDivisible(num1, num2) {
+//     if (num1 % num2 === 0){
+//     console.log(true)
+//         }else if (num1 % num2 !== 0){
+//           console.log(false)
+//         }
+//     }
+
+
+ //console.log(isDivisible(4, 2)) // => true
+ //console.log(isDivisible(9, 3)) // => true
+ //console.log(isDivisible(15, 4)) // => false
