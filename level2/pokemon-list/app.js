a393da33c69c4dcddf6c767eb9ae1a869e392814
alton peel <altonpeel123@gmail.com>
2023-09-04 11:04:25 -0400

    finished Json deli

---
+++ level2/pokemon-list/app.js
@@ -0,0 +1,19 @@
+const xhr = new XMLHttpRequest()
+        // method  // url                     // async?
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+
+xhr.send()
+
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200){
+        const JSONdata = xhr.responseText
+        const data = JSON.parse(JSONdata)
+        console.log(data.results)
+    }
+}
+
+function showData(arr){
+    const h1 = document.createElement('h1')
+    h1.textContent = pokemon.name
+    document.body.appendChild(h1)
+}
