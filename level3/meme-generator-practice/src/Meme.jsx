7537296a4f4da13c3ee7e633945316f136d4e66a
alton peel <altonpeel123@gmail.com>
2023-11-04 10:31:00 -0400

    Finished level 3 capstone and caeser cipher

--- level3/meme-generator-practice/src/Meme.jsx
+++ level3/meme-generator-practice/src/Meme.jsx
@@ -1,79 +1,119 @@
-import React from "react"
-import memeData from "./memeData"
 
-export default function Meme(){
+import React, { useEffect, useState } from "react";
+import SavedMemeList from "./SavedMemeList";
 
-    const [meme, setMeme] = React.useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
-    })
-    const [allMemes, setAllMemes] = React.useState([])
-    
-    React.useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemes(data.data.memes))
-    }, [])
+export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+    memeId: ''
+  });
 
-    function getMemeImage() {
-        const randomNumber = Math.floor(Math.random() * allMemes.length)
-        const url = allMemes[randomNumber].url
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            randomImage: url
-        }))
-        
+  //list of all meme photos
+  const [allMemes, setAllMemes] = useState([])
+
+  // get new meme photo
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length)
+    const url = allMemes[randomNumber].url
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        randomImage: url
+    }))
+  }
+  //Api call to get a new photo for meme
+  useEffect(() => {
+      fetch("https://api.imgflip.com/get_memes")
+          .then(res => res.json())
+          .then(data => setAllMemes(data.data.memes))
+          .catch(err => console.log(err))
+  }, [])
+  
+  //update meme state
+  function handleChange(event) {
+      const {name, value} = event.target
+      setMeme(prevMeme => ({
+          ...prevMeme,
+          [name]: value
+      }))
+  }
+
+  const [savedMemes, setSavedMemes] = useState([]);
+
+  // save button
+  function save() {
+    const memeId = Date.now();
+    setSavedMemes((prevMemes) => {
+      return [...prevMemes, {...meme, memeId: memeId}]
+    });
+  }
+
+  console.log(savedMemes)
+  //delete button
+  function deleteMeme(memeId) {
+    setSavedMemes(prevMemes => prevMemes.filter(meme => meme.memeId !== memeId))
+  }
+  //Edit button
+  const saveEditedMeme = (memeId, edit) => {
+    setSavedMemes(prevMemes => prevMemes.map(meme => meme.memeId !== memeId ? meme : edit));
     }
 
-    function handleChange(event) {
-        const {name, value} = event.target
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]: value
-        }))
-    }
 
+  
 
-    return(
-        <main>
-            <div className="form">
+  const savedMemeList = savedMemes.map((meme) => {
+    return (
+      <SavedMemeList
+        key={meme.memeId}
+        meme={meme}
+        deleteMeme={() => deleteMeme(meme.memeId)}
+        saveEditedMeme={saveEditedMeme}
+      />
+    );
+  });
 
-                <input 
-                    className="form--input" 
-                    type="text" 
-                    placeholder="Top Text"
-                    name="topText"
-                    value={meme.topText}
-                    onChange={handleChange}
-                />
+  return (
+    <main>
+      <div className="form">
+        <input 
+          className="form--input" 
+          type="text" 
+          placeholder="Top Text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input 
+          className="form--input" 
+          type="text" 
+          placeholder="Bottom Text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+        <button 
+          className="memebtn" 
+          onClick={getMemeImage}>
+            Get your Meme image
+        </button>
+      </div>
 
-                <input 
-                    className="form--input" 
-                    type="text" 
-                    placeholder="Bottom Text"
-                    name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                />
-
-                <button 
-                className="memebtn" 
-                onClick={getMemeImage}
-                >Create your Meme
-                </button>
-
-            </div>
-
-            <div className="meme">
-                <div className="meme--container">
-                    <img src={meme.randomImage} className="meme--image" />
-                    <h2 className="meme--text top">{meme.topText}</h2>
-                    <h2 className="meme--text bottom">{meme.bottomText}</h2>
-                </div>
-            </div>
-        </main>
-    )
+      <div className="meme">
+        <div className="meme--container">
+          <img src={meme.randomImage} className="meme--image" alt="Meme" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
+      </div>
+            <button className="saveEdit" onClick={save}> Save Meme </button>
+      
+      <div>
+        {savedMemeList}
+      </div>
+    </main>
+  );
 }
+
 
 
---
+++ level3/meme-generator-practice/src/SavedMemeList.jsx
@@ -0,0 +1,64 @@
+
+import React, { useState } from "react";
+
+export default function SavedMemeList({ meme, deleteMeme, saveEditedMeme }) {
+
+    const [editing, setEditing] = useState(false);
+    const [edit, setEdit] = useState({
+        topText: meme.topText,
+        bottomText: meme.bottomText,
+        randomImage: meme.randomImage,
+        memeId: meme.memeId
+    })
+
+    const handleEditClick = () => {
+      setEditing(true);
+    };
+  
+    const handleCancelClick = () => {
+      setEditing(false);
+    };
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setEdit(prev => {
+            return {
+                ...prev, 
+                [name] : value
+            }
+        })
+    }
+    
+  
+    const handleSaveClick = () => {
+      saveEditedMeme(meme.memeId, edit);
+      setEditing(false);
+    };
+
+
+  return (
+    <>
+      <div className="meme">
+        <div className="meme--container">
+            <img src={meme.randomImage} className="meme--image" alt="Meme" />
+            <h2 className="meme--text top">{editing ? <input type="text" name="topText" value={edit.topText} onChange={handleChange} /> : meme.topText}</h2>
+            <h2 className="meme--text bottom">{editing ? <input type="text" name="bottomText" value={edit.bottomText} onChange={handleChange} /> : meme.bottomText}</h2>
+        </div>
+      </div>
+    {/* if editing is true provide delete and edit buttons */}
+      {!editing && (
+      <div className="memeBtns">
+        <button className="saveEdit" onClick={deleteMeme}>Delete</button>
+        <button className="saveEdit" onClick={handleEditClick}>Edit</button>
+      </div>
+      )}
+    {/* if editing is true provides save and cancel buttons */}
+      {editing && (
+        <div>
+          <button className="saveEdit"onClick={handleSaveClick}>Save</button>
+          <button className="saveEdit"onClick={handleCancelClick}>Cancel</button>
+        </div>
+      )}
+    </>
+  );
+}
